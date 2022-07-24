36ff940b30521c1cf3648ded5ba1103b2ab6cdcd
Rizkie Pratama <MayoSandwich@fedoraserver.vm>
2022-07-25 04:48:25 +0700

    Split gallery js components conflicting with Navbar/Global js script

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,24 +1,4 @@
 $(document).ready(function(){
-  initTab();
-
-  var grid = document.querySelector('.portofolio-wrapper');
-  var msnry = new Masonry( grid, {
-    itemSelector: '.gallery-item',
-    percentPosition: true,
-  });
-
-  document.querySelectorAll('.gallery-item').forEach(el => el.addEventListener('click', (e) => {
-    e.preventDefault();
-    const lightBox = new Lightbox(el, {
-      keyboard: true,
-      showArrows: true,
-      wrapping: true,
-      size: 'fullscreen',
-    });
-    lightBox.show();
-    $('.ratio').css('background-color', '');
-  }));
-
   $('.navbar-toggler').click(function() {
     var nav_menu = $('.navbar-collapse');
     var nav_toggler = $('.navbar-toggler');
@@ -39,34 +19,4 @@ $(document).ready(function(){
       nav_toggler.children().removeClass('fa-bars').addClass('fa-times');
     }
   });
-
-  $(window).on('hashchange', function() {
-    try {
-        var current = document.querySelectorAll('.portofolio-wrapper');
-        for(var i = 0; i < current.length; i++) {
-          current[i].classList.remove('active');
-        }
-    } catch(err) {
-        console.log(err.message);
-        console.log("Continuing with next iteration...");
-    }
-
-    initTab();
-  });
-
-  function initTab() {
-    var target = [];
-    var tabHash = document.location.hash;
-    if(tabHash === '') tabHash = '#street';
-
-    /*
-    if(tabHash === '#all') target = ['.street-photog','.cosplay-photog'];
-    if(tabHash === '#street') target = ['.street-photog'];
-    if(tabHash === '#cosplay') target = ['.cosplay-photog'];*/
-
-    var qselector = document.querySelectorAll(tabHash);
-    for(var i = 0; i < qselector.length; i++){
-      qselector[i].classList.toggle('active');
-    }
-  }
 });
